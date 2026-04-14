a3d63afdd3cc5ba9265b76d6cab8afc45768d03a
Juan Lagoria <lagoria79@gmail.com>
2026-04-14 18:12:04 -0300

    add fisrt structure of the project

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main.ts',
+    '!**/*.module.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@prisma/client$': '<rootDir>/prisma/generated/prisma/client',
+  },
+};
